72546a2633fc7a682df698900569ce57cb705cac
emilgrupp16 <emil.johansson62@hotmail.com>
2024-12-09 13:08:52 +0100

    s

---
+++ Temperature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABB3PAR4
+{
+    internal class Temperature
+    {
+        private Array({0}, {1});
+    }
+}
